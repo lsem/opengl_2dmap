ba516bad7f1d479c5ab68744ac3562e74bf37c30
Liubomyr Semkiv <liubomyrsemkiv@Liubomyr.Semkiv>
2021-07-12 13:33:32 +0300

    my ex: add one more object which drawn via struct and two pointers

--- main.cpp
+++ main.cpp
@@ -59,6 +59,30 @@ const char* fragment_shader = R"shader(
 		}
 	)shader";
 
+const char* exp_vertex_shader = R"shader(
+		#version 330 core
+		layout (location = 0) in vec3 aPos;
+		layout (location = 1) in float col;
+
+		out float color;
+
+		void main()
+		{
+			color = col;
+		    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+		}
+)shader";
+const char* exp_fragment_shader = R"shader(
+		#version 330 core
+		in float color;
+		out vec4 FragColor;
+
+		void main()
+		{
+		    FragColor = vec4(color, color, color, 1.0f);
+		}
+	)shader";
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
   // make sure the viewport matches the new window dimensions; note that width
   // and height will be significantly larger than specified on retina displays.
@@ -99,7 +123,6 @@ int main() {
   unsigned v_shader = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(v_shader, 1, &vertex_shader, NULL);
   glCompileShader(v_shader);
-
   int success;
   char infoLog[512];
   glGetShaderiv(v_shader, GL_COMPILE_STATUS, &success);
@@ -135,10 +158,74 @@ int main() {
 
   float dx1 = -0.25;
   float dx2 = +0.25;
-  float vertices1[] = {dx1 + -0.25f, -0.25f,     0.0f,  dx1 + 0.25f, -0.25f,
-                       0.0f,         dx1 + 0.0f, 0.25f, 0.0f};
+  float z = 0.0f;
+  float vertices1[] = {dx1 + -0.25f, -0.25f,    z,  dx1 + 0.25f, -0.25f,
+                       z,         dx1 + 0.0f, 0.25f, z};
   float vertices2[] = {dx2 + -0.25f, -0.25f,     0.0f,  dx2 + 0.25f, -0.25f,
                        0.0f,         dx2 + 0.0f, 0.25f, 0.0f};
+
+// ------------------------------------
+#pragma pack(0)
+  struct Vertex {  // todo: is it packed?
+    Vertex(float x, float y, float z, float col) : x(x), y(y), z(z), col(col) {}
+    float x, y, z, col;
+  };
+  Vertex vertices3[] = {{-0.25f, -0.25f, 0.0f, 1.0f},
+                        {0.25f, -0.25f, 0.0f, 0.5f},
+                        {0.0f, 0.25f, 0.0f, 0.0f}};
+  unsigned experimental_vao, experimental_vbo;
+  glGenVertexArrays(1, &experimental_vao);
+  glGenBuffers(1, &experimental_vbo);
+  glBindVertexArray(experimental_vao);
+  glBindBuffer(GL_ARRAY_BUFFER, experimental_vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices3), &vertices3[0],
+               GL_STATIC_DRAW);
+  // data vec3
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+                        (void*)offsetof(Vertex, x));
+  glEnableVertexAttribArray(0);
+  // color
+  glVertexAttribPointer(1, 1, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+                        (void*)offsetof(Vertex, col));
+  glEnableVertexAttribArray(1);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);  // unbind
+  glBindVertexArray(0);              // unbind
+
+  unsigned exp_v_shader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(exp_v_shader, 1, &exp_vertex_shader, NULL);
+  glCompileShader(exp_v_shader);
+  glGetShaderiv(exp_v_shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(exp_v_shader, 512, NULL, infoLog);
+    std::cout << "exp_v_shader COMPILATION_FAILED\n" << infoLog << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  unsigned exp_f_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(exp_f_shader, 1, &exp_fragment_shader, NULL);
+  glCompileShader(exp_f_shader);
+  glGetShaderiv(exp_f_shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(exp_f_shader, 512, NULL, infoLog);
+    std::cout << "exp_f_shader COMPILATION_FAILED\n" << infoLog << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  unsigned int shaderProgram2 = glCreateProgram();
+  glAttachShader(shaderProgram2, exp_v_shader);
+  glAttachShader(shaderProgram2, exp_f_shader);
+  glLinkProgram(shaderProgram2);
+  glGetProgramiv(shaderProgram2, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(shaderProgram2, 512, NULL, infoLog);
+    std::cout << "ERROR:: shaderProgram2 Link failed\n" << infoLog << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glDeleteShader(exp_v_shader);
+  glDeleteShader(exp_f_shader);
+
+  // ------------------------------------
 
   unsigned int VAO1, VBO1, VAO2, VBO2;
 
@@ -177,19 +264,22 @@ int main() {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    if (g_wireframe_mode && g_wireframe_mode != g_prev_wireframe_mode) {
-      glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    } else if (!g_wireframe_mode && g_wireframe_mode != g_prev_wireframe_mode) {
-      glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    if (g_wireframe_mode != g_prev_wireframe_mode) {
+      if (g_wireframe_mode) {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+      } else {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+      }
     }
 
     glUseProgram(shaderProgram);
-    glBindVertexArray(VAO1);  // can be not bound each time but often is like //
-                              // that in realistic program
+    glBindVertexArray(VAO1);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindVertexArray(VAO2);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
-    glBindVertexArray(VAO2);  // can be not bound each time but often is like //
-                              // that in realistic program
+    glUseProgram(shaderProgram2);
+    glBindVertexArray(experimental_vao);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(window);
